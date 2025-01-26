9638b57794c9672cc1fc5e9ac3dcc5dcf7512558
Kenny Chin <114026407+kennychin1994@users.noreply.github.com>
2025-01-26 03:02:04 +0000

    updated images for Allies, Plunder, Adventures, and Rising Sun

--- src/components/CardSelection.tsx
+++ src/components/CardSelection.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import { Card } from "./types";
